6f88de2b1f99ddb19ea390fae0d2eb119cdf9e6f
wkdthf21 <wkdthf21@naver.com>
2020-05-14 01:34:36 +0900

    feat: add Post Entity Class

---
+++ src/main/java/com/naver/hackday/snstimeline/post/domain/Post.java
@@ -0,0 +1,35 @@
+package com.naver.hackday.snstimeline.post.domain;
+
+import com.naver.hackday.snstimeline.common.BaseTimeEntity;
+import com.naver.hackday.snstimeline.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "posts")
+@Entity
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+
+    @Column(name = "contents", length = 2000)
+    private String contents;
+
+    @Builder
+    public Post(User user, String contents){
+        this.user = user;
+        this.contents = contents;
+    }
+
+}
