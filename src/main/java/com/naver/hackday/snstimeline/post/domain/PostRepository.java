653aa31f7f922cbb3a9547a09d2eb57735a3b202
wkdthf21 <wkdthf21@naver.com>
2020-05-14 14:07:40 +0900

    feat: add UpdateContents in PostRepository

--- src/main/java/com/naver/hackday/snstimeline/post/domain/PostRepository.java
+++ src/main/java/com/naver/hackday/snstimeline/post/domain/PostRepository.java
@@ -1,6 +1,13 @@
 package com.naver.hackday.snstimeline.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	@Modifying
+	@Query(value = "update Post p set p.contents = :#{#post.contents} WHERE p.id = :#{#post.id}", nativeQuery=false)
+	Integer updateContents(@Param("post") Post post);
 }
