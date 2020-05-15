228e29890c4dc555b120ea328edf33525603509a
wkdthf21 <wkdthf21@naver.com>
2020-05-15 18:19:33 +0900

    feat: add delete post in PostService

--- src/main/java/com/naver/hackday/snstimeline/post/service/PostService.java
+++ src/main/java/com/naver/hackday/snstimeline/post/service/PostService.java
@@ -1,5 +1,7 @@
 package com.naver.hackday.snstimeline.post.service;
 
+import java.util.OptionalInt;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
 import com.naver.hackday.snstimeline.common.exception.NotFoundException;
+import com.naver.hackday.snstimeline.post.domain.Post;
 import com.naver.hackday.snstimeline.post.domain.PostRepository;
 import com.naver.hackday.snstimeline.post.dto.PostSaveRequestDto;
 import com.naver.hackday.snstimeline.user.domain.User;
@@ -26,6 +29,20 @@ public class PostService {
 		User user = this.getUserEntity(postSaveRequestDto.getUserId(), "user_id");
 		// Create Post
 		postRepository.save(postSaveRequestDto.toEntity(user));
+	}
+
+	@Transactional
+	public void deletePost(Long id){
+		// Find Post
+		// Exception : Not Found Post
+		Post post = this.getPostEntity(id);
+		// Delete Post
+		postRepository.deletePostById(post);
+	}
+
+	private Post getPostEntity(Long id){
+		return postRepository.findById(id)
+			.orElseThrow(() -> new NotFoundException("post_id", "존재하지 않는 포스팅입니다."));
 	}
 
 	private User getUserEntity(String userId, String field) {
