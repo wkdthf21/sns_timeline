6b6e2748550c520f8e32837fe18f124898325338
wkdthf21 <wkdthf21@naver.com>
2020-05-15 16:31:48 +0900

    feat: add find user using user id in UserRepository Class

--- src/main/java/com/naver/hackday/snstimeline/user/domain/UserRepository.java
+++ src/main/java/com/naver/hackday/snstimeline/user/domain/UserRepository.java
@@ -1,6 +1,9 @@
 package com.naver.hackday.snstimeline.user.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUserId(String userId);
 }
