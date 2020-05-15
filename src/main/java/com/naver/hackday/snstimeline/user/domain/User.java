06daa298683d58283906238b22f4411099cfc98d
wkdthf21 <wkdthf21@naver.com>
2020-05-16 01:48:55 +0900

    feat: add temp userId unique setting in User class

--- src/main/java/com/naver/hackday/snstimeline/user/domain/User.java
+++ src/main/java/com/naver/hackday/snstimeline/user/domain/User.java
@@ -20,7 +20,7 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String userId;
 
     private String nickname;
