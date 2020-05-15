52a4e37913cbae9a0662929d3b915048a3985f24
wkdthf21 <wkdthf21@naver.com>
2020-05-15 16:31:03 +0900

    feat: add common exception package

---
+++ src/main/java/com/naver/hackday/snstimeline/common/exception/ControllerExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.naver.hackday.snstimeline.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ExceptionResponseDto> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		BindingResult bindingResult = exception.getBindingResult();
+
+		FieldError fieldError = bindingResult.getFieldErrors().get(0);
+		ExceptionResponseDto responseDto = ExceptionResponseDto.builder()
+			.field(fieldError.getField())
+			.message(fieldError.getDefaultMessage())
+			.build();
+
+		return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<ExceptionResponseDto> notFoundException(NotFoundException exception) {
+		ExceptionResponseDto responseDto = ExceptionResponseDto.builder()
+			.field(exception.getField())
+			.message(exception.getMessage())
+			.build();
+
+		return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
+	}
+}
