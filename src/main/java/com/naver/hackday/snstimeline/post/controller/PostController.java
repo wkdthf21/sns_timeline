795263ff5f3a922f5379a6952eabfa8b6512a7c3
wkdthf21 <wkdthf21@naver.com>
2020-05-15 18:19:56 +0900

    feat: add post delete api

--- src/main/java/com/naver/hackday/snstimeline/post/controller/PostController.java
+++ src/main/java/com/naver/hackday/snstimeline/post/controller/PostController.java
@@ -2,11 +2,14 @@ package com.naver.hackday.snstimeline.post.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -25,7 +28,7 @@ public class PostController {
 	// 포스트 작성
 	@ApiOperation("포스트 작성")
 	@ApiResponses({
-		@ApiResponse(code = 200, message = "포스팅 성공"),
+		@ApiResponse(code = 201, message = "포스팅 성공"),
 		@ApiResponse(code = 404, message = "존재하지 않는 유저"),
 		@ApiResponse(code = 500, message = "서버 에러")
 	})
@@ -34,7 +37,25 @@ public class PostController {
 		// Upload Post
 		postService.uploadPost(postSaveRequestDto);
 		// Return
-		return new ResponseEntity<>("Success Post Upload", HttpStatus.OK);
+		return new ResponseEntity<>("Success Post Upload", HttpStatus.CREATED);
+	}
+
+	// 포스트 삭제
+	@ApiOperation("포스트 삭제")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "post_id", value = "포스팅 PK", required = true, dataType = "string", paramType = "path", defaultValue = ""),
+	})
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "포스팅 삭제 성공"),
+		@ApiResponse(code = 404, message = "존재하지 않는 포스팅"),
+		@ApiResponse(code = 500, message = "서버 에러")
+	})
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{post_id}")
+	public ResponseEntity deletePost(@PathVariable(value = "post_id") Long id){
+		// Delete Post
+		postService.deletePost(id);
+		// Return
+		return new ResponseEntity<>("Success Post Delete", HttpStatus.OK);
 	}
 
 
