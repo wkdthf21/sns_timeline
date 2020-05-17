dbf9825ca07b26bef476166fc11d5c5509872988
HyeranShin <hyeran9712@naver.com>
2020-05-17 19:59:00 +0900

    resolve: 충돌 해결

--- src/main/java/com/naver/hackday/snstimeline/user/domain/UserRepository.java
+++ src/main/java/com/naver/hackday/snstimeline/user/domain/UserRepository.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<List<User>> findByUserIdContainingOrNicknameContaining(String userId, String nickname);
-	  Optional<User> findByUserId(String userId);
+
+    Optional<User> findByUserId(String userId);
 }
