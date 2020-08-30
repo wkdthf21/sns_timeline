6b77fc7ec33b7ad3a7106665f71920dbf27c2aee
wkdthf21 <wkdthf21@naver.com>
2020-08-30 23:01:58 +0900

    test: add delete filefunction after each test

--- src/test/java/com/naver/hackday/snstimeline/post/service/FileServiceTest.java
+++ src/test/java/com/naver/hackday/snstimeline/post/service/FileServiceTest.java
@@ -2,57 +2,73 @@ package com.naver.hackday.snstimeline.post.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.naver.hackday.snstimeline.SnsTimelineApplication;
+import com.naver.hackday.snstimeline.config.FileUploadConfig;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SnsTimelineApplication.class)
 public class FileServiceTest {
-
-	@Autowired
+	
+	private FileUploadConfig fileUploadProperty;
 	private FileService fileService;
-	private String savedFileName;
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	private String filename;
+	private ClassPathResource cpr;
+	private MultipartFile multipartFile;
+	
+	
 	@Before
+	public void before() {
+		
+		fileUploadProperty = new FileUploadConfig();
+		fileUploadProperty.setDirectory("./uploads");
+		fileService = new FileService(fileUploadProperty);
+	}
+	
+	@After
+	public void after() {
+		fileService.deleteFile(filename);
+	}
+	
+	@Test
 	public void saveFile() throws Exception {
-
+			
 		// given
-		ClassPathResource cpr = new ClassPathResource("test_img.png");
-
-		MultipartFile multipartFile = new MockMultipartFile( cpr.getPath(), "test_img.png", null,
+		cpr = new ClassPathResource("test_img.png");
+		multipartFile = new MockMultipartFile( cpr.getPath(), "test_img.png", null,
 			cpr.getInputStream());
 
 		// when
-		savedFileName = fileService.saveFile(multipartFile);
+		filename = fileService.saveFile(multipartFile);
 
 		// then
-		assertThat(savedFileName).isNotNull();
-
+		assertThat(filename).isNotNull();
 	}
 
 	@Test
-	public void downloadFileAsResource() {
+	public void downloadFileAsResource() throws IOException {
+		// given
+		cpr = new ClassPathResource("test_img.png");
+		multipartFile = new MockMultipartFile( cpr.getPath(), "test_img.png", null,
+			cpr.getInputStream());
+		filename = fileService.saveFile(multipartFile);
+		
 		// when
-		Resource resource = fileService.downloadFileAsResource(savedFileName);
+		Resource resource = fileService.downloadFileAsResource(filename);
 		// then
 		assertThat(resource).isNotNull();
 		assertThat(resource.exists()).isTrue();
-		assertThat(resource.getFilename()).contains(savedFileName);
+		assertThat(resource.getFilename()).contains(filename);
 	}
 
 
